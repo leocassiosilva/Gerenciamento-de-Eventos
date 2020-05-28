0047080dc10ff896ab6bbc75197b16197f5a35cb
leocassiosilva <leocassiosilva1234@gmail.com>
2020-05-28 14:50:48 -0300

    first commit

---
+++ src/main/java/com/evento/sistemaevento/GestaoEventoApplication.java
@@ -0,0 +1,13 @@
+package com.evento.sistemaevento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestaoEventoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestaoEventoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/evento/sistemaevento/GestaoEventoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.evento.sistemaevento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestaoEventoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
