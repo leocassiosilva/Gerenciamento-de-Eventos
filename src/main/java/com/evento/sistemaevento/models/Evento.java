70f40c58fd6592e156cf42f5fa2f0a26ff71d995
leocassiosilva <leocassiosilva1234@gmail.com>
2020-06-04 19:34:52 -0300

    Novas Atualizações

--- src/main/java/com/evento/sistemaevento/models/Evento.java
+++ src/main/java/com/evento/sistemaevento/models/Evento.java
@@ -1,6 +1,21 @@
 package com.evento.sistemaevento.models;
 
-public class Evento {
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+
+
+@Entity
+public class Evento implements Serializable{
+	
+	private static final long serialVersionUID = 1L;
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private long codigo;
 	private String nome;
 	private String local;
 	private String data;
@@ -8,6 +23,12 @@ public class Evento {
 	
 	
 	
+	public long getCodigo() {
+		return codigo;
+	}
+	public void setCodigo(long codigo) {
+		this.codigo = codigo;
+	}
 	public String getNome() {
 		return nome;
 	}
