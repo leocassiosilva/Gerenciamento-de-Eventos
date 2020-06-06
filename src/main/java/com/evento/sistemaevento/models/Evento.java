b070f38e294b8e84b0e4c8f0820909932b8568c6
leocassiosilva <leocassiosilva1234@gmail.com>
2020-06-06 19:45:50 -0300

    Subindo

--- src/main/java/com/evento/sistemaevento/models/Evento.java
+++ src/main/java/com/evento/sistemaevento/models/Evento.java
@@ -4,7 +4,6 @@ import java.io.Serializable;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 
@@ -14,7 +13,7 @@ public class Evento implements Serializable{
 	
 	private static final long serialVersionUID = 1L;
 	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
+	@GeneratedValue
 	private long codigo;
 	private String nome;
 	private String local;
