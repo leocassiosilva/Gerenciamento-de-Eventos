b4b2643d115401a88f9972447368bea6e10b8f5e
leocassiosilva <leocassiosilva1234@gmail.com>
2020-06-08 14:18:05 -0300

    Subindo

--- src/main/java/com/evento/sistemaevento/controllers/EventoController.java
+++ src/main/java/com/evento/sistemaevento/controllers/EventoController.java
@@ -1,12 +1,37 @@
 package com.evento.sistemaevento.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.evento.sistemaevento.models.Evento;
+import com.evento.sistemaevento.repository.EventoRepository;
 
 @Controller
 public class EventoController {
-	@RequestMapping("/cadastrarEvento")
+	
+	@Autowired
+	private EventoRepository er;
+	
+	@RequestMapping(value="/cadastrarEvento", method = RequestMethod.GET)
 	public String form() {
 		return "evento/formEvento";
+	}
+	
+	@RequestMapping(value="/cadastrarEvento", method = RequestMethod.POST)
+	public String form(Evento evento) {
+		er.save(evento);
+		return "redirect:/cadastrarEvento";
+	}
+	
+	@RequestMapping("/eventos")
+	public ModelAndView listaEventos() {
+		ModelAndView mv = new ModelAndView("index.html");
+		Iterable<Evento> eventos = er.findAll();
+		mv.addObject("eventos", eventos);
+		return mv;
+		
 	}
 }
---
+++ src/main/java/com/evento/sistemaevento/repository/EventoRepository.java
@@ -0,0 +1,9 @@
+package com.evento.sistemaevento.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.evento.sistemaevento.models.Evento;
+
+public interface EventoRepository extends CrudRepository<Evento, String>{
+
+}
